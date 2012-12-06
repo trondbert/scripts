a4752a5215c9422f20c28faba2463f491cba82c7
Trond Valen <trondvalen@gmail.com>
2012-12-06 20:08:09 +0100

    Mer krangling

--- fileReadAndWrite.groovy
+++ fileReadAndWrite.groovy
@@ -1,10 +1,23 @@
 
-def outFile = ""
-new File("foo.txt").eachLine { line->
-    if (line =~ /create/) {
-        def m = line =~ /create .*package +(.*)/
-        outFile = m[0][1] + ".sql"
-        println outFile
+class Foo {
+    def outFilename = ""
+    def fos = null
+
+public static void main(args) {
+    new Foo().runMe()
+}
+
+void runMe() {
+    new File("foo.txt").eachLine { line->
+        if (line =~ /create/) {
+            def m = line =~ /create .*package +(.*)/
+            fos?.flush()
+            outFilename = m[0][1] + ".sql"
+            fos = new FileOutputStream(outFilename)
+            println ( "new file " + outFileName)
+        }
+        fos.write(line)    
     }
-    
+    fos.flush()
+}
 }
